5ba7fd512a1bcaf86a20e66a8ef13366deb0da4e
abdegenius <johnnieabijah2000@gmail.com>
2023-03-24 13:20:30 +0100

    Updates

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -33,13 +33,6 @@
 								class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
 								>Attendance</a
 							>
-
-							<a
-								href="#"
-								class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-								>Zones</a
-							>
-
 							<a
 								href="#"
 								class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
--- src/components/MobileNav.svelte
+++ src/components/MobileNav.svelte
@@ -7,27 +7,21 @@
 	<div class="space-y-1 px-2 py-3 sm:px-3">
 		<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
 		<a
-			href="#"
+			href="/dashboard"
 			class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
 			aria-current="page">Dashboard</a
 		>
 
 		<a
-			href="#"
+			href="/dashboard/members"
 			class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-			>Team</a
+			>Members</a
 		>
 
 		<a
-			href="#"
+			href="/dashboard/attendance"
 			class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-			>Projects</a
-		>
-
-		<a
-			href="#"
-			class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-			>Calendar</a
+			>Attendance</a
 		>
 
 		<a
